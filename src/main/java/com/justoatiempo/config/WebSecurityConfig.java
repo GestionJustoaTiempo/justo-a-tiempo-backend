49e7dda286d29e805a6af187918e5fd2704a39ef
Cristian Rodriguez <cristianrodriguez@MacBook-Air-de-Cristian.local>
2026-01-31 00:31:06 -0300

    Actualizar WebSecurityConfig para Spring Security

--- src/main/java/com/justoatiempo/config/WebSecurityConfig.java
+++ src/main/java/com/justoatiempo/config/WebSecurityConfig.java
@@ -1,5 +1,10 @@
 package com.justoatiempo.config;
 
+package com.justoatiempo.config;
+
+// PRUEBA CAMBIO WEBSECURITYCONFIG
+
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
