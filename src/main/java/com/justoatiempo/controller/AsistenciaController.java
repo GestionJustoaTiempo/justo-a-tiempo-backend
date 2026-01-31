38bbd0ba6b42686b0a853a08811b2429f6bbd2f9
Cristian Rodriguez <cristianrodriguez@MacBook-Air-de-Cristian.local>
2026-01-31 15:50:23 -0300

    Cambiar ruta de asistencias a singular

--- src/main/java/com/justoatiempo/controller/AsistenciaController.java
+++ src/main/java/com/justoatiempo/controller/AsistenciaController.java
@@ -10,7 +10,7 @@ import java.util.List;
 import java.util.Optional;
 
 @RestController
-@RequestMapping("/api/asistencias")
+@RequestMapping("/api/asistencia")
 @CrossOrigin(origins = {
         "http://localhost:3000",
         "http://localhost:8080",
